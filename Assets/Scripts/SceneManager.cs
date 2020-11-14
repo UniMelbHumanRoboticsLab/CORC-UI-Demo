456817b3db881f0e60a305a72dbc6281385bfd3d
vcrocher <vcrocher@unimelb.edu.au>
2020-11-15 08:50:45 +1100

    Fixed cursor mapping

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -31,13 +31,13 @@ public class SceneManager : MonoBehaviour
             Status.text += "\tt: " + Robot.State["t"][0].ToString("####.00") + "\n";
             Status.text += "\tX:" + Robot.State["X"][0].ToString("0.000") + "\t" + Robot.State["X"][1].ToString("0.000") + "\t" + Robot.State["X"][2].ToString("0.000") + "\n";
             Status.text += "\tdX:" + Robot.State["dX"][0].ToString("0.00") + "\t" + Robot.State["dX"][1].ToString("0.00") + "\t" + Robot.State["dX"][2].ToString("0.00") + "\n";
-            Status.text += "\tF:" + Robot.State["F"][0].ToString("00.00") + "\t" + Robot.State["F"][1].ToString("00.00") + "\t" + Robot.State["F"][2].ToString("00.00") + "\n";
+            Status.text += "\tF:" + Robot.State["F"][0].ToString("00.0") + "\t" + Robot.State["F"][1].ToString("00.0") + "\t" + Robot.State["F"][2].ToString("00.0") + "\n";
 
             float scale = 1000;
-            Vector3 Origin = new Vector3(0, 0, 0);
-            Cursor.transform.position = new Vector3((float)Robot.State["X"][0], -(float)Robot.State["X"][2], (float)Robot.State["X"][1])*scale+Origin;
-            Vector3 force = new Vector3((float)Robot.State["F"][0], -(float)Robot.State["F"][2], (float)Robot.State["F"][1]);
-            float force_scale = 40;
+            Vector3 Origin = new Vector3(0, 0, -500);
+            Cursor.transform.position = new Vector3((float)Robot.State["X"][1], (float)Robot.State["X"][2], -(float)Robot.State["X"][0])*scale+Origin;
+            Vector3 force = new Vector3((float)Robot.State["F"][1], (float)Robot.State["F"][2], -(float)Robot.State["F"][0]);
+            float force_scale = 10;
             Arrow.transform.localPosition = new Vector3(0, 0, force.magnitude / force_scale);
             Arrow.transform.localScale = new Vector3(0.2f, force.magnitude / force_scale, 0.2f);
             Cursor.transform.LookAt(Cursor.transform.position + force);
